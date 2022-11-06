4f3faee4af0799e364a7138bacf3355177cf6aad
LitenPanJun <xiaopanjun233@163.com>
2022-11-06 12:07:48 +0800

    Added AllowlistFile.cs(The class about allowlist.json),Modified Events.cs, and FantasyTownAllowlist.csproj

---
+++ AllowlistFile.cs
@@ -0,0 +1,22 @@
+﻿namespace FantasyTownAllowlist
+{
+    public class AllowlistFile
+    {
+        /// <summary>
+        /// 玩家名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 玩家Xuid
+        /// </summary>
+        public int XUID { get; set; }
+        /// <summary>
+        /// 玩家最后登录时间
+        /// </summary>
+        public int LastJoin { get; set; }
+    }
+    //public class Root
+    //{
+    //    public ListMain ListMain { get; set; }
+    //}
+}
--- Events.cs
+++ Events.cs
@@ -1,30 +1,59 @@
 ﻿using LLNET.Event;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using LLNET.Logger;
+using Newtonsoft.Json;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FantasyTownAllowlist
 {
     internal class Events
     {
-        Allowlist allowlist = new();
+        Allowlist allowlistMgr = new();
+        /// <summary>
+        /// 判断玩家是否在白名单中
+        /// </summary>
+        /// <param name="Name">玩家名</param>
+        /// <returns></returns>
+        private bool PlayerInAllowlist(string Name)
+        {
+            Console.WriteLine(allowlistMgr.Read());
+            List<AllowlistFile>? list = JsonConvert.DeserializeObject<List<AllowlistFile>>(allowlistMgr.Read());
+            if (list == null || list.Count == 0)
+                return false;
+            else
+            {
+                foreach (var item in list)
+                {
+                    Console.WriteLine("2");
+                    if (item.Name == Name)
+                        return true;
+                }
+                return false;
+            }
+        }
         public void playerJoin()
         {
+            //预进入，检查白名单
             PlayerPreJoinEvent.Subscribe(e =>
             {
                 string Name = e.Player.Name;
-                if (allowlist.PlayerInAllowlist(Name) == true)
-                    return true;
-                else return false;
+                Console.WriteLine(Name);
+                if (PlayerInAllowlist(Name) == false)
+                {
+                    e.Player.Kick("你还没有服务器白名单，请联系你所在的服务器的管理员！");
+                    return false;
+                }
+                return true;
             });
+            //已进入，检查白名单（适配WeaterDog）
             PlayerJoinEvent.Subscribe(e =>
             {
                 string Name = e.Player.Name;
-                if (allowlist.PlayerInAllowlist(Name) == true)
-                    return true;
-                else return false;
+                if (PlayerInAllowlist(Name) == false)
+                {
+                    e.Player.Kick("你还没有服务器白名单，请联系你所在的服务器的管理员！");
+                    return false;
+                }
+                return true;
             });
         }
     }
