78bb89bbea1d313620cf5571382f8a98a65b8b76
LitenPanJun <xiaopanjun233@163.com>
2022-11-06 12:05:28 +0800

    Rename FileWriteAndRead.cs to Allowlist.cs,Modified PluginMain.cs

--- Allowlist.cs
+++ Allowlist.cs
@@ -1,25 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using LLNET.Logger;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FantasyTownAllowlist
 {
     internal class Allowlist
     {
-        FlieWriteAndRead far = new();
-        /// <summary>
-        /// 判断玩家是否在白名单中
-        /// </summary>
-        /// <param name="Name">玩家名</param>
-        /// <returns></returns>
-        public bool PlayerInAllowlist(string Name)
+        string FilePath = ".\\plugins\\FantasyTown\\Allowlist\\allowlist.json";
+        string OldFile = ".\\plugins\\FantasyTown\\allowlist\\allowlist.json";
+        Logger logger = new("FantasyTownAllowlist");
+        public void Start()
         {
-            string v = far.Read(Name);
-            if (v != "" && v != null)
-                return true;
-            else return false;
+            if (File.Exists(OldFile) == false && File.Exists(FilePath) == false)
+            {
+                logger.Info.WriteLine("检测到白名单文件不存在，正在创建……");
+                logger.Info.WriteLine("Allowlist file does not exist, creating...");
+                Directory.CreateDirectory(".\\plugins\\FantasyTown\\Allowlist");
+                Create();
+            }
+            else if (File.Exists(OldFile) == true && File.Exists(FilePath) == false)
+            {
+                logger.Info.WriteLine("暂不支持旧版本白名单文件自动迁移，请手动迁移");
+                logger.Info.WriteLine("检测到新版本白名单文件不存在，正在创建……");
+                logger.Info.WriteLine("Automatic migration of old allowlist files is not supported, please migrate manually");
+                logger.Info.WriteLine("New allowlist file does not exist, creating...");
+                Create();
+            }
+            else if (File.Exists(OldFile) == false && File.Exists(FilePath) == true)
+            {
+                logger.Info.WriteLine("检测到白名单文件不存在，正在创建……");
+                logger.Info.WriteLine("Allowlist file does not exist, creating...");
+                Directory.CreateDirectory(".\\plugins\\FantasyTown\\Allowlist");
+                Create();
+            }
+        }
+        /// <summary>
+        /// 创建白名单文件
+        /// </summary>
+        /// <param name="File">文件路径</param>
+        /// <returns></returns>
+        public bool Create()
+        {
+            try
+            {
+                List<AllowlistFile> allowlist = new List<AllowlistFile>();
+                string json = JsonConvert.SerializeObject(allowlist);
+                using (FileStream fs = new(FilePath, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+                {
+                    using (StreamWriter sw = new(fs, Encoding.UTF8))
+                    {
+                        sw.Write(json);
+                        sw.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error.WriteLine("白名单文件创建出错：" + e.Message);
+                logger.Error.WriteLine("Error creating allowlist file: " + e.Message);
+                return false;
+                throw;
+            }
+            logger.Info.WriteLine("白名单文件创建成功！");
+            logger.Info.WriteLine("Allowlist file created!");
+            return true;
+        }
+        /// <summary>
+        /// 写白名单
+        /// </summary>
+        /// <param name="Player">玩家名</param>
+        /// <returns></returns>
+        public bool Write(string Player)
+        {
+            return true;
+        }
+        /// <summary>
+        /// 读白名单
+        /// </summary>
+        /// <returns></returns>
+        public string Read()
+        {
+            using (FileStream fs = new(".\\plugins\\FantasyTown\\Allowlist\\allowlist.json", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader reader = new(fs, Encoding.UTF8))
+                {
+                    return reader.ReadToEnd().ToString();
+                }
+            }
         }
     }
 }
--- PluginMain.cs
+++ PluginMain.cs
@@ -5,21 +5,21 @@ namespace FantasyTownAllowlist;
 [PluginMain("FantasyTownAllowlist")]
 public class FantasyTownAllowList : IPluginInitializer
 {
-    Logger logger = new();
+    Logger logger = new("FantasyTownAllowlist");
+    Allowlist allowlistMgr = new();
+    Events e = new();
     //元信息
     public Dictionary<string, string> MetaData => new()
     {
-        {"Auther","Liten_PanJun"},
-        {"作者","小潘菌"}
+        {"Auther","Liten_PanJun"}
     };
     //介绍
-    public string Introduction => "Allow servers with online-mode=false (such as WeaterDog downstream servers) to enable white list plugin.";
+    public string Introduction => "Allow servers with online-mode=false to enable allowlist.";
     //版本
     public Version Version => new(1, 5, 0);
     //载入
     public void OnInitialize()
     {
-        string Path = ".\\plugins\\FantasyTown\\allowlist\\allowlist.json";
         Task.Run(() =>
         {
             Thread.Sleep(5500);
@@ -27,13 +27,8 @@ public class FantasyTownAllowList : IPluginInitializer
             logger.info.WriteLine("FantasyTownAllowlist已加载！");
             logger.info.WriteLine("Plugin version: " + Version.ToString());
             logger.info.WriteLine("插件版本：" + Version.ToString());
-            if (File.Exists(Path) == false)
-            {
-                logger.Info.WriteLine("检测到白名单文件不存在，正在创建……");
-                logger.Info.WriteLine("Allowlist file does not exist, creating...");
-                Directory.CreateDirectory(".\\plugins\\FantasyTown\\allowlist");
-                File.Create(Path);
-            }
+            allowlistMgr.Start();
         });
+        e.playerJoin();
     }
 }
